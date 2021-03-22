6b7a6eff34ced538da42b6029c683dad19c87afb
Juan <juanjo.dc@outlook.com>
2021-03-22 17:51:05 -0500

    arreglados problemas con logica e interfaz

--- interfaz.py
+++ interfaz.py
@@ -1,107 +1,13 @@
 import tkinter as tk
 from tkinter import *
+import sys
+sys.path.append("./modulos/")
+from modulos import logica_interfaz as lg 
 
+cadena_operacion=""
+tipo_resultado=""
+pos_par=0
 
-#inicio funciones previas ejecutar logica
-def encontrar_operandos(cadena_operaciones): 
-    pos_operaciones=[]
-    tam_cadena=len(cadena_operaciones) 
-    caracter=''
-    pos_actual=0
-    while(pos_actual<tam_cadena):
-        caracter=cadena_operaciones[pos_actual] 
-        if(caracter=='+'):
-            pos_operaciones.append(pos_actual) 
-        elif(caracter=='-'):
-            pos_operaciones.append(pos_actual) 
-        elif(caracter=='/'):
-            pos_operaciones.append(pos_actual) 
-        elif(caracter=='*'):
-            if pos_actual+1<tam_cadena:
-                if cadena_operaciones[pos_actual+1]=='*':
-                    pos_actual+=2
-                    print(pos_actual)
-                else: 
-                    pos_operaciones.append(pos_actual)
-        pos_actual=pos_actual+1
-    return pos_operaciones
-
-def separar_cadena(cadena):  
-    vector_op=encontrar_operandos(cadena) #contiene orden operandos
-    inicio=0 
-    fin=vector_op[0] 
-    pos_vector=0
-    cadena_separada=[]
-    tam_vector=len(vector_op)
-    while(pos_vector<tam_vector):
-        cadena_separada.append(cadena[inicio:fin]) 
-        cadena_separada.append(cadena[fin])
-        if(pos_vector+1==tam_vector):
-            cadena_separada.append(cadena[fin+1:])
-        inicio=fin
-        pos_vector+=1
-        if pos_vector<tam_vector:
-            fin=vector_op[pos_vector]
-        
-        
-            
-
-    return cadena_separada
-def verificar_numeros(cadena_sep): 
-    """
-    Esta funcion determina si los numeros cuentan con su respectivo
-    sistema numerico , en caso de que un numero no cuente con el 
-    sistema numerico indicado, señalara un error
-    en caso que los numeros se encuentren indicados correctamente 
-    esta funcion retornara : negativo para errores, numeros sin sistema,
-    lista con los tipos de sistemas encontrados
-
-    """
-    parent_inicial=0
-    parent_final=0
-    tipo=""
-    pos=0
-    tam=len(cadena_sep)
-    error=False
-    cadena=""
-    tipos_num=[]
-    while(pos<tam):
-        caena=cadena_sep[pos]
-        if(len(cadena)>1):
-            parent_inicial=cadena.find('(')
-            if(parent_inicial==-1):
-                error=True
-                break
-            else:
-                parent_final=cadena.find(')')
-                tipos_num.append(cadena[parent_inicial:parent_final])
-                cadena=list(cadena)
-                #eliminamos el tipo de numero de la cadena
-                for parent_inicial in range(parent_final+1): 
-                    cadena.pop(parent_inicial)
-                cadena=str(cadena)
-                cadena_sep[pos]=cadena
-        pos+=1
-    return error,cadena_sep,tipos_num
-
-
-
-def ejecutar_cadena(cadena):
-    if(cadena==""):
-        entrada_operacion.set("ERROR LOGICO")
-    else:
-        #indentacion
-        cadena_separada=separar_cadena(cadena)
-        error,cadena_separada,tipos_num=verificar_numeros(cadena_separada)
-        if(error):
-            entrada_operacion.set("ERROR LOGICO")
-
-    
-
-    
-
-
-#Fin funciones previa ejecutar logica 
 
 def registrar_entrada(caracter):
    
@@ -119,14 +25,25 @@ def eliminar_caracter():
 
 def eventos(event):
     texto=event.widget.cget("text")
+    global cadena_operacion
+    
     if(texto=="="):
         #Ejecucion de operaciones
-        ejecutar_cadena(entrada_operacion.get())
+        cadena_operacion=entrada_operacion.get()
+        entrada_operacion.set("En que sistema desea recibir el numero?")
+        
     elif(texto=="AC"):
         #Limpiar entrada 
         limpiar_entrada()
     elif(texto=="DEL"): 
         eliminar_caracter()
+    elif(texto=="Enter"):
+        tipo_resultado=entrada_operacion.get() 
+        pos_par=tipo_resultado.find('(')
+        tipo_resultado=tipo_resultado[pos_par:] 
+        entrada_operacion.set(lg.ejecutar_cadena(cadena_operacion,tipo_resultado))
+
+        
     else:
         #agrega caracter
         registrar_entrada(texto)
@@ -267,6 +184,11 @@ btpow.bind('<Button-1>',eventos)
 bteq=tk.Button(ventana,text="=")
 bteq.place(x=280,y=210,width=40)
 bteq.bind('<Button-1>',eventos)
+
+
+btenter=tk.Button(ventana,text="Enter") 
+btenter.place(x=240,y=240,width=80)
+btenter.bind('<Button-1>',eventos)
 #Fin botones operacionales 
 
 
